5bda967e26fd7c332ccf3325203f0d8bbb826a38
Nahid Aghababaeyan <nahid.aut@gmail.com>
2023-05-10 14:53:40 -0400

    Fix Bugs

--- freMTPL2freq.r
+++ freMTPL2freq.r
@@ -139,15 +139,8 @@ data <- data %>% rename(R82 = '1', R22 = '2', R72 = '3', R31 = '4', R91 = '5', R
 # Check renaming
 head(data)
 
-
-
-
-
 library(caret)
 library(neuralnet)
-
-# Load the updated dataset
-data <- read.csv("./data/freMTPL2freq_updated.csv")
 
 # Split the dataset into training and validation sets
 set.seed(123)
@@ -155,47 +148,15 @@ trainIndex <- createDataPartition(data$AvgSalary, p = 0.8, list = FALSE)
 train <- data[trainIndex, ]
 valid <- data[-trainIndex, ]
 
-# Scale the input variables to have zero mean and unit variance
-preProcValues <- preProcess(train[,c("IDpol","ClaimNb","Exposure","Area","VehPower","VehAge","DrivAge","BonusMalus","AvgSalary")], method = c("center", "scale"))
-
-train[,c("IDpol","ClaimNb","Exposure","Area","VehPower","VehAge","DrivAge","BonusMalus","AvgSalary")] <- predict(preProcValues, train[,c("IDpol","ClaimNb","Exposure","Area","VehPower","VehAge","DrivAge","BonusMalus","VehBrand","AvgSalary")])
-valid[,c("IDpol","ClaimNb","Exposure","Area","VehPower","VehAge","DrivAge","BonusMalus","AvgSalary")] <- predict(preProcValues, valid[,c("IDpol","ClaimNb","Exposure","Area","VehPower","VehAge","DrivAge","BonusMalus","VehBrand","AvgSalary")])
-
-numeric <- function(area) {
-  if (area == "A") {
-    return(0)
-  } else if (area == "B") {
-    return(1)
-  } else if (area == "C") {
-    return(2)
-  } else if (area == "D") {
-    return(3)
-  } else if (area == "E") {
-    return(4)
-  } else if (area == "F") {
-    return(5)
-  } else if (area == "G") {
-    return(6)
-  } else if (area == "H") {
-    return(7)
-  } else {
-    return(8)
-  }
-}
-
-
-
-# Apply the function to the 'Area' column to create the 'avgsalary' column
-train$Area <- sapply(train$Area, numeric)
-
 # Check the data types of each column
 str(train)
 
 # Check for missing or invalid values
 summary(train)
 
-# Remove any rows with missing or invalid values
-train <- na.omit(train)
+
+
+
 
 # Check the formula for correctness and completeness
 formula <- as.formula("AvgSalary ~ IDpol + ClaimNb + Exposure + Area + VehPower + VehAge + DrivAge + BonusMalus ")
@@ -208,33 +169,6 @@ nn <- neuralnet(formula, data = train, hidden = c(5,3), linear.output = FALSE)
 # Check the structure and weights of the neural network
 str(nn)
 nn$weights
-
-numeric <- function(area) {
-  if (area == "A") {
-    return(0)
-  } else if (area == "B") {
-    return(1)
-  } else if (area == "C") {
-    return(2)
-  } else if (area == "D") {
-    return(3)
-  } else if (area == "E") {
-    return(4)
-  } else if (area == "F") {
-    return(5)
-  } else if (area == "G") {
-    return(6)
-  } else if (area == "H") {
-    return(7)
-  } else {
-    return(8)
-  }
-}
-
-# Apply the function to the 'Area' column to create the 'avgsalary' column
-valid$Area <- sapply(valid$Area, numeric)
-
-valid <- na.omit(valid)
 
 # Make predictions on the validation set
 predictions <- predict(nn, valid[,c("IDpol","ClaimNb","Exposure","Area","VehPower","VehAge","DrivAge","BonusMalus")])
@@ -268,17 +202,3 @@ df <- cbind(df, area_matrix)
 
 # Remove the original 'area' column
 df$area <- NULL
-
-# //////////////////////////
-
-# data <- data.frame(
-#   Outcome = seq(1,100,by=1),
-#   Variable = sample(c("Red","Green","Blue"), 100, replace = TRUE)
-# )
-# one_hot in mltools package
-# library(mltools)
-# library(data.table)
-
-# newdata <- one_hot(as.data.table(data))
-# data$Variable <- as.factor(data$Variable)
-# newdata <- one_hot(as.data.table(data))
